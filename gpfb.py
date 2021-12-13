e4eeecf09fafdead6d126b5c475a1fb4aef68543
elsultansilvio <janclaes40@gmail.com>
2021-12-13 22:01:15 +0100

    updates

--- gpfb.py
+++ gpfb.py
@@ -6,10 +6,10 @@ start_timer = time.time()
 STARTUP_WAIT = 10  # programma gaat bij opstart x seconden wachten alvorens te starten (voor bv. VPN)
 counter = 0 # programma gaat na counter_max re-initialiseren (kijken of inputfile veranderd is)
 counter_max = 13
-sleep_time = 5  # wachttijd in seconden tussen status checks
+sleep_time = 5  # wachttijd in seconden tussen status check `LastModified` & eventueele `CopyTo()`
 
 
-def __init__() -> tuple[str,DataFrame]:
+def __init__(initFailCounter=0) -> tuple[str,DataFrame]:
     ''' Initialiseer, lees input (excel) in met `SOURCE` en `DESTINATION` van files\n
         Test of file `SOURCE` en `DESTINATION` toegankelijk zijn (voor elke file), neem enkel files mee zonder error\n
         Zet flags `RECENT_FILE_UPDATE` is True & `LAST_TRANSFER` is 0 zodat files meegenomen worden in `CopyTo` flow\n
@@ -22,52 +22,72 @@ def __init__() -> tuple[str,DataFrame]:
 
     # reeks concatenates voor PATH en FILE; overbodige columns verwijderen
     SOURCE_inputfile = PathFile(STARTUP_source_PATH,STARTUP_source_FILE)
-    STARTUP_DF = pd.read_excel(SOURCE_inputfile, engine='openpyxl')    
-    STARTUP_DF['SOURCE'] = STARTUP_DF.apply(lambda x: PathFile(x['SOURCE_PATH'],x['SOURCE_FILE']), axis=1) 
-    STARTUP_DF['DESTINATION'] = STARTUP_DF.apply(lambda x: PathFile(x['DESTINATION_PATH'],x['DESTINATION_FILE']), axis=1)   
-    STARTUP_DF.drop(columns=['SOURCE_PATH','SOURCE_FILE','DESTINATION_FILE'], inplace=True) 
-    
-    # bool die aangeeft of file recent is aangepast
-    # Staat bij opstart op True om copy te forceren
-    STARTUP_DF['RECENT_FILE_UPDATE'] = True     
-    # unixtimestamp van laatste transfer van file
-    # Staat bij opstart op 0 om copy te forceren
-    STARTUP_DF['LAST_TRANSFER'] = 0
+    STARTUP_DF = pd.read_excel(SOURCE_inputfile, engine='openpyxl')  
+    STARTUP_DF = STARTUP_DF[STARTUP_DF['SOURCE_FILE'].notna()]  # neem enkel records waar `SOURCE_FILE` ingevuld is
+    if STARTUP_DF['SOURCE_FILE'].isna().all():
+        time.sleep(STARTUP_WAIT)
+        initFailCounter += 1
+        if initFailCounter == 10:
+            print('exit, input file niet gevonden')
+            exit()
+        __init__(initFailCounter)  # probeer opnieuw
+    else:
+        STARTUP_DF['SOURCE'] = STARTUP_DF.apply(lambda x: PathFile(x['SOURCE_PATH'],x['SOURCE_FILE']), axis=1) 
+        STARTUP_DF['DESTINATION'] = STARTUP_DF.apply(lambda x: PathFile(x['DESTINATION_PATH'],x['DESTINATION_FILE']), axis=1)   
+        STARTUP_DF.drop(columns=['SOURCE_PATH','SOURCE_FILE','DESTINATION_FILE'], inplace=True) 
+        
+        # bool die aangeeft of file recent is aangepast
+        # Staat bij opstart op True om copy te forceren
+        STARTUP_DF['RECENT_FILE_UPDATE'] = True     
+        # unixtimestamp van laatste transfer van file
+        # Staat bij opstart op 0 om copy te forceren
+        STARTUP_DF['LAST_TRANSFER'] = 0
 
-    STARTUP_DF = Get_Status_Wrapper(STARTUP_DF)
-    # neem enkel records zonder error in source en/of destination
-    STARTUP_DF_NoError = STARTUP_DF[(STARTUP_DF['ERROR_SOURCE'] == 0) & (STARTUP_DF['ERROR_DESTINATION'] == 0)].copy()
+        STARTUP_DF = Get_Status_Wrapper(STARTUP_DF)
+        # neem enkel records zonder error in source en/of destination
+        STARTUP_DF_NoError = STARTUP_DF[(STARTUP_DF['ERROR_SOURCE'] == 0) & (STARTUP_DF['ERROR_DESTINATION'] == 0)].copy()
 
-    STARTUP_DF_NoError = CopyTo(STARTUP_DF_NoError)                   
-    return SOURCE_inputfile, STARTUP_DF_NoError
+        STARTUP_DF_NoError = CopyTo(STARTUP_DF_NoError)  
+        return SOURCE_inputfile, STARTUP_DF_NoError
 
 def re__init__(SOURCE, OLD_STARTUP_DF_NoError) -> DataFrame:
-    ''' re-Initialiseer, lees input (excel) in met `SOURCE` en `DESTINATION` van files\n
-        Test of file `SOURCE` en `DESTINATION` toegankelijk zijn (voor elke file), neem enkel files mee zonder error\n
-        Zet flags `RECENT_FILE_UPDATE` is True & `LAST_TRANSFER` is 0 zodat NIEUWE files meegenomen worden in `CopyTo` flow\n
+    ''' re-initialiseer, lees input (excel) in met `SOURCE` en `DESTINATION` van files\n
+        Test of files `SOURCE` en `DESTINATION` toegankelijk zijn (voor elke file), neem enkel files mee zonder error\n
+        Zet flag `RECENT_FILE_UPDATE` = True & `LAST_TRANSFER` = 0 zodat NIEUWE files meegenomen worden in `CopyTo` flow\n
         Return
         ------
-        Path naar bronfile (excel) `SOURCE_inputfile` str & DataFrame `STARTUP_DF_NoError`'''
-    # lees start up excel opnieuw in, concatenates voor PATH en FILE
-    # & overbodige columns verwijderen
-    STARTUP_DF = pd.read_excel(SOURCE, engine='openpyxl')
-    STARTUP_DF['SOURCE'] = STARTUP_DF.apply(lambda x: PathFile(x['SOURCE_PATH'],x['SOURCE_FILE']), axis=1)
-    STARTUP_DF['DESTINATION'] = STARTUP_DF.apply(lambda x: PathFile(x['DESTINATION_PATH'],x['DESTINATION_FILE']), axis=1)   
-    STARTUP_DF.drop(columns=['SOURCE_PATH','SOURCE_FILE','DESTINATION_FILE'], inplace=True)  
+        Path naar bronfile (excel) `SOURCE_inputfile` str & DataFrame `STARTUP_DF_NoError`\n
+        Exception
+        ---------
+        re-initialiseren faalt -> initialiseer `__init__()`'''
+    try:
+        # reeks concatenates voor PATH en FILE; overbodige columns verwijderen
+        STARTUP_DF = pd.read_excel(SOURCE, engine='openpyxl')
+        STARTUP_DF['SOURCE'] = STARTUP_DF.apply(lambda x: PathFile(x['SOURCE_PATH'],x['SOURCE_FILE']), axis=1)
+        STARTUP_DF['DESTINATION'] = STARTUP_DF.apply(lambda x: PathFile(x['DESTINATION_PATH'],x['DESTINATION_FILE']), axis=1)   
+        STARTUP_DF.drop(columns=['SOURCE_PATH','SOURCE_FILE','DESTINATION_FILE'], inplace=True)  
 
-    STARTUP_DF = Get_Status_Wrapper(STARTUP_DF)   
+        STARTUP_DF = Get_Status_Wrapper(STARTUP_DF)   
 
-    # bool die aangeeft of file recent is aangepast
-    # Staat bij re__init__ op True om copy te forceren
-    STARTUP_DF['RECENT_FILE_UPDATE'] = True
-    # unixtimestamp van laatste transfer van file
-    # Staat bij opstart op 0 om copy te forceren
-    STARTUP_DF['LAST_TRANSFER'] = 0       
+        # bool die aangeeft of file recent is aangepast
+        # Staat bij re__init__ op True om copy te forceren
+        STARTUP_DF['RECENT_FILE_UPDATE'] = True
+        # unixtimestamp van laatste transfer van file
+        # Staat bij opstart op 0 om copy te forceren
+        STARTUP_DF['LAST_TRANSFER'] = 0       
 
-    STARTUP_DF_NoError = STARTUP_DF[(STARTUP_DF['ERROR_SOURCE'] == 0) & (STARTUP_DF['ERROR_DESTINATION'] == 0)].copy()  
-    STARTUP_DF_NoError.loc[OLD_STARTUP_DF_NoError.index] = OLD_STARTUP_DF_NoError
-      
-    return STARTUP_DF_NoError
+        # neem enkel records zonder error in source en/of destination
+        STARTUP_DF_NoError = STARTUP_DF[(STARTUP_DF['ERROR_SOURCE'] == 0) & (STARTUP_DF['ERROR_DESTINATION'] == 0)].copy()  
+        # vervang op niveau van index (=rij) van excel de opnieuw ingelezen excel
+        # indien file van row 1 naar 2 gaat, en row 1 blanco is
+        # gaan we hier een error krijgen
+        columns = ['RECENT_FILE_UPDATE','LAST_TRANSFER']
+        STARTUP_DF_NoError[columns].loc[OLD_STARTUP_DF_NoError.index] = OLD_STARTUP_DF_NoError[columns]
+        
+        return STARTUP_DF_NoError
+    except:
+        print("re__init__() failed")
+        __init__()  # complete herstart
 
 def PathFile(path, file) -> str:
     ''' concatenate `PATH` en `FILE`. Inputformat: bv. `SOURCE_PATH` = C:/Users/xxx/MIJNFOLDER\n
@@ -114,12 +134,13 @@ def Get_Status_Destination(df) -> DataFrame:
 
         return df
     except:
-        print('niet elke destination directory gevonden')
+        print('ERROR DESTINATION: niet voor elke file gevonden')
         for dest in df['DESTINATION_PATH']:
             try:
                 os.listdir(dest)
                 ERROR_DESTINATION.append(False)
             except:
+                print('ERROR DESTINATION: {0}'.format(dest))
                 ERROR_DESTINATION.append(True)
 
         df['ERROR_DESTINATION'] = ERROR_DESTINATION
@@ -132,12 +153,15 @@ def Get_Status_Source(df) -> DataFrame:
         Return
         ------
         DataFrame met `ERROR_SOURCE` bool en `LAST_MODIFIED` unixtimestamp\n
-            bij error `SOURCE`: ERROR_SOURCE = True en LAST_MODIFIED= 0'''
+            bij error `SOURCE`: ERROR_SOURCE = True en LAST_MODIFIED= 0\n
+        Exception
+        ------
+        '''
     try:
         df['LAST_MODIFIED'] = df.apply(lambda x: LastModified(x['SOURCE']), axis = 1)
         df['ERROR_SOURCE'] = False
     except:
-        print('niet elke file gevonden') # TD: error handler    
+        print('ERROR SOURCE: niet elke file gevonden') # TD: error handler    
         LAST_MODIFIED, ERROR_SOURCE = RowByRow_Updater(df)
         df['LAST_MODIFIED'] = LAST_MODIFIED
         df['ERROR_SOURCE'] = ERROR_SOURCE
@@ -150,18 +174,26 @@ def RowByRow_Updater(df) -> list:
         
         Return
         ------
-        List `LAST_MODIFIED` unixtimestamp, List `ERROR_SOURCE` bool'''
-    LAST_MODIFIED = []
-    ERROR_SOURCE = []
-    for source in df['SOURCE']: 
-        try:           
-            LAST_MODIFIED.append(LastModified(source))
-            ERROR_SOURCE.append(False)
-        except:
-            LAST_MODIFIED.append(0)
-            ERROR_SOURCE.append(True)
-    
-    return LAST_MODIFIED, ERROR_SOURCE
+        List `LAST_MODIFIED` unixtimestamp, List `ERROR_SOURCE` bool\n
+        Exception
+        ------
+        `RowByRow_updater()` faalt (bv. geen locaties in bronfile) -> initialiseer `__init__()`
+        '''
+    try:
+        LAST_MODIFIED = []
+        ERROR_SOURCE = []
+        for source in df['SOURCE']: 
+            try:           
+                LAST_MODIFIED.append(LastModified(source))
+                ERROR_SOURCE.append(False)
+            except:
+                LAST_MODIFIED.append(0)
+                ERROR_SOURCE.append(True)
+        
+        return LAST_MODIFIED, ERROR_SOURCE
+    except TypeError:   # geen enkele `SOURCE` en `DESTINATION` pair gevonden
+        print("RowByRow_updated failed")
+        __init__()  # complete herstart
 
 def RecentUpdated(df) -> bool:
     ''' kijkt of `LAST_MODIFIED` groter is dan `LAST_TRANSFER`\n 
